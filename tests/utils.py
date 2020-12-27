5d29f1d78c5f0c08438b747661bfd3207696b6ec
Maxim Avanov <maxim.avanov@gmail.com>
2020-12-27 08:35:48 +0000

    remove spec-related tests

--- tests/utils.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import json
-from openapi_client_generator.oas_spec import Specification, parse_spec
-
-
-def load_spec(s: Path) -> Specification:
-    with s.open() as fd:
-        return parse_spec(json.load(fd))
