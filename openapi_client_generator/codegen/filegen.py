cfe22a2178af28a347cec2771b48fb4e598e22de
Maxim Avanov <maxim.avanov@gmail.com>
2021-01-01 21:02:23 +0000

    typed context

--- openapi_client_generator/codegen/filegen.py
+++ openapi_client_generator/codegen/filegen.py
@@ -1,32 +1,69 @@
 """ Generates client file structure
 """
 from itertools import chain
-from string import digits
 from pathlib import Path
 from shutil import copytree
-from typing import NamedTuple, Mapping, Any, Generator, Tuple
+from typing import NamedTuple, Mapping, Generator, Tuple, Iterable, Union
 from pkg_resources import get_distribution
 
-from pyrsistent import pmap
 from inflection import underscore
 import openapi_type as oas
 
 from . import templates
 from ..info import DISTRIBUTION_NAME, PACKAGE_NAME
+from ..transformers import SpecMeta, openapi_to_codegen_metadata
 
 
-README = Path('README.md')
-MANIFEST = Path('MANIFEST.in')
-SETUP_PY = Path('setup.py')
+README       = Path('README.md')
+MANIFEST     = Path('MANIFEST.in')
+SETUP_PY     = Path('setup.py')
 REQUIREMENTS = Path('requirements') / 'minimal.txt'
 
-EMPTY_CONTEXT: Mapping[str, Any] = pmap()
+
+class EmptyContext(NamedTuple):
+    pass
+
+
+class EndpointContext(NamedTuple):
+    package_name: str
+    endpoint_url: str
+    params_type: str
+    request_type: str
+    response_type: str
+    headers_type: str
+
+
+class ServiceContext(NamedTuple):
+    import_names: Iterable[str]
+
+
+class ReadmeContext(NamedTuple):
+    client_name: str
+
+
+class ManifestContext(NamedTuple):
+    package_name: str
+
+
+class SetupContext(NamedTuple):
+    client_name: str
+
+
+EMPTY_CONTEXT = EmptyContext()
+
+
+Context = Union[ EndpointContext
+               , ServiceContext
+               , ReadmeContext
+               , ManifestContext
+               , SetupContext
+               , EmptyContext ]
 
 
 class Binding(NamedTuple):
     layout: Path
     template: templates.Template
-    context: Mapping[str, Any]
+    context: Context
 
 
 Endpoints = Mapping[Binding, oas.Operation]
@@ -44,15 +81,6 @@ class ProjectLayout(NamedTuple):
     requirements: Binding
 
 
-class EndpointContext(NamedTuple):
-    package_name: str
-    endpoint_url: str
-    params_type: str
-    request_type: str
-    response_type: str
-    headers_type: str
-
-
 def client_layout(spec: oas.OpenAPI, root: Path, name: str) -> ProjectLayout:
     """
     :param spec: parsed OpenAPI Spec
@@ -60,33 +88,32 @@ def client_layout(spec: oas.OpenAPI, root: Path, name: str) -> ProjectLayout:
     :param name: client python package name
     :return: client layout representation as a type
     """
+    meta = openapi_to_codegen_metadata(spec)
     py_name = underscore(name)
     client_root = root / py_name
     endpoints_root = client_root / "service"
     common_root = client_root / "common"
 
-    endpoints = endpoints_bindings(spec, py_name, endpoints_root)
+    endpoints = endpoints_bindings(meta, py_name, endpoints_root)
 
     return ProjectLayout(
         root=root,
         client_root=client_root,
         common_root=common_root,
         endpoints_root=endpoints_root,
-        readme=Binding(root / README, templates.README, {
-            'client_name': name,
-            'client_caption_separator': '=' * len(name),
-        }),
-        manifest=Binding(root / MANIFEST, templates.MANIFEST, {'package_name': py_name}),
-        setup_py=Binding(root / SETUP_PY, templates.SETUP_PY, {'client_name': name}),
+        readme=Binding(
+            root / README, templates.README, ReadmeContext(client_name=name,)
+        ),
+        manifest=Binding(root / MANIFEST, templates.MANIFEST, ManifestContext(package_name=py_name)),
+        setup_py=Binding(root / SETUP_PY, templates.SETUP_PY, SetupContext(client_name=name)),
         requirements=Binding(root / REQUIREMENTS, templates.REQUIREMENTS, EMPTY_CONTEXT),
         endpoints=endpoints
     )
 
 
-def endpoints_bindings(spec: oas.OpenAPI, package_name: str, endpoints_root: Path) -> Endpoints:
+def endpoints_bindings(meta: SpecMeta, package_name: str, endpoints_root: Path) -> Endpoints:
     endpoints = {}
-    for path, item in spec.paths.items():
-        pth = api_path_to_filepath(path)
+    for pth, item in meta.paths.items():
         for name, method in _iter_supported_methods(item):
             target = endpoints_root / pth / f'{name}.py'
             ctx = EndpointContext(
@@ -97,50 +124,17 @@ def endpoints_bindings(spec: oas.OpenAPI, package_name: str, endpoints_root: Pat
                 response_type=templates.RESPONSE_TYPE.render({}),
                 headers_type=templates.HEADERS_TYPE.render({}),
             )
-            endpoints[Binding(target, templates.ENDPOINT, pmap(ctx._asdict()))] = method
+            endpoints[Binding(target, templates.ENDPOINT, ctx)] = method
             # make sure there's __init__ in every sub-package
             for sub_pkg in (x for x in target.parents if x > endpoints_root):
                 endpoints[Binding(sub_pkg / '__init__.py', templates.ENDPOINT_INIT, EMPTY_CONTEXT)] = method
     return endpoints
 
 
-def api_path_to_filepath(api_path: str, sep: str = '/') -> Path:
-    """
-    :param api_path: URL path
-    :param sep: separator symbol used in API path
-    """
-    segments = [pythonize_path_segment(x) for x in api_path.split(sep) if x.strip()]
-    if not segments:
-        segments = ['root']
-    return Path('/'.join(segments))
-
-
-def pythonize_path_segment(seg: str) -> str:
-    placeholder = {'{', '}'}
-    remove = {'.', ','} | placeholder
-    is_placeholder = False
-    final = []
-    for char in seg:
-        if char in placeholder:
-            is_placeholder = True
-            continue
-        if char in remove:
-            continue
-        final.append(char)
-    rv = underscore(''.join(final))
-    if is_placeholder:
-        rv = f'by_{rv}'
-    else:
-        # version tags are usually numeric
-        if rv[0] in digits:
-            rv = f'v{rv}'
-    return rv
-
-
 def generate_from_layout(l: ProjectLayout) -> None:
     for binding in [l.readme, l.manifest, l.setup_py, l.requirements]:
         _generate_file(binding)
-    _generate_file(Binding(l.client_root / '__init__.py', templates.PACKAGE_INIT, {}))
+    _generate_file(Binding(l.client_root / '__init__.py', templates.PACKAGE_INIT, EMPTY_CONTEXT))
     _copy_common_library(l.common_root)
     _generate_endpoints(l.endpoints)
     _generate_imports(l.endpoints_root)
@@ -151,14 +145,12 @@ def _generate_endpoints(e: Endpoints) -> None:
         _generate_file(py_binding)
 
 
-def _generate_imports(root: Path) -> None:
-    init_name = '__init__.py'
+def _generate_imports(root: Path, init_name: str = '__init__.py') -> None:
     for init in chain(root.rglob(init_name), [root / init_name]):
         import_names = (mod.name.replace('.py', '') for mod in init.parent.iterdir() if mod.name != init_name)
+        ctx = ServiceContext(import_names=import_names)._asdict()
         with init.open('w') as f:
-            templates.SERVICE_INIT.stream({
-                'import_names': import_names
-            }).dump(f)
+            templates.SERVICE_INIT.stream(ctx).dump(f)
 
 
 def _generate_file(binding: Binding) -> None:
@@ -166,7 +158,7 @@ def _generate_file(binding: Binding) -> None:
         binding.layout.parent.mkdir(exist_ok=True, parents=True)
     binding.layout.touch(exist_ok=False)
     with binding.layout.open('w') as f:
-        binding.template.stream(binding.context).dump(f)
+        binding.template.stream(binding.context._asdict()).dump(f)
 
 
 def _copy_common_library(common_root: Path) -> None:
--- openapi_client_generator/transformers/__init__.py
+++ openapi_client_generator/transformers/__init__.py
@@ -1,2 +1,61 @@
 """ A collection of functions that transform OpenAPI spec to data structures convenient for codegen.
 """
+from pathlib import Path
+from string import digits
+from typing import NamedTuple, Mapping
+
+import openapi_type as oas
+from inflection import underscore
+from pyrsistent import pmap
+
+
+class SpecMeta(NamedTuple):
+    """ A post-processed OpenAPI specification that is convenient to use for codegen
+    """
+    spec: oas.OpenAPI
+    """ original spec
+    """
+    paths: Mapping[Path, oas.PathItem]
+
+
+def openapi_to_codegen_metadata(spec: oas.OpenAPI) -> SpecMeta:
+    paths = pmap({
+        api_path_to_filepath(path): item for path, item in spec.paths.items()
+    })
+    return SpecMeta(
+        spec=spec,
+        paths=paths
+    )
+
+
+def api_path_to_filepath(api_path: str, sep: str = '/') -> Path:
+    """
+    :param api_path: URL path
+    :param sep: separator symbol used in API path
+    """
+    segments = [pythonize_path_segment(x) for x in api_path.split(sep) if x.strip()]
+    if not segments:
+        segments = ['root']
+    return Path('/'.join(segments))
+
+
+def pythonize_path_segment(seg: str) -> str:
+    placeholder = {'{', '}'}
+    remove = {'.', ','} | placeholder
+    is_placeholder = False
+    final = []
+    for char in seg:
+        if char in placeholder:
+            is_placeholder = True
+            continue
+        if char in remove:
+            continue
+        final.append(char)
+    rv = underscore(''.join(final))
+    if is_placeholder:
+        rv = f'by_{rv}'
+    else:
+        # version tags are usually numeric
+        if rv[0] in digits:
+            rv = f'v{rv}'
+    return rv
