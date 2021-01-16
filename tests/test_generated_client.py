0617bd3752936d3e2a37d1c7b3dfd820eb303da6
Maxim Avanov <maxim.avanov@gmail.com>
2021-01-16 20:55:58 +0000

    Fix tests

--- tests/test_generated_client.py
+++ tests/test_generated_client.py
@@ -7,19 +7,13 @@ import petstore_full
 ])
 def test_client(service_url):
     client = petstore_full.common.http.Client(service_url=service_url)
-    endpoint1 = petstore_full.service.pet.post
-    action1 = endpoint1.call(
+    endpoint = petstore_full.service.pet.find_by_tags.get
+    action = endpoint.call(
         client,
-        request=endpoint1.Request(
-            name='pet',
-            photo_urls=[],
-        )
+        query=endpoint.Query(
+            tags=['dog']
+        ),
+        headers=endpoint.Headers()
     )
-
-    endpoint2 = petstore_full.service.pet.find_by_tags.get
-    action2 = endpoint2.call(
-        client,
-        query=endpoint2.Query(
-            tags=['term']
-        )
-    )
+    assert isinstance(action, list)
+    assert isinstance(action[0], endpoint.Pet)
