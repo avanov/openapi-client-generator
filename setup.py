7ead451341bf3ac6486c1f86a38aee73d5922f0b
Maxim Avanov <maxim.avanov@gmail.com>
2021-01-18 11:23:45 +0000

    Release 1.0.5

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with (here / 'README.rst').open() as f:
 # ----------------------------
 
 setup(name='openapi-client-generator',
-      version='1.0.4',
+      version='1.0.5',
       description='OpenAPI Client Generator',
       long_description=README,
       classifiers=[
